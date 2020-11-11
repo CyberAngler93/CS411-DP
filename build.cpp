b37689adab189e4967d4b24fe50a2c446d4f56a3
CyberAngler93 <mperry37@alaska.edu>
2020-11-11 12:53:34 -0900

    feat: build init, added test program

---
+++ build.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Matt on 11/11/2020.
+//
+
---
+++ build.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Matt on 11/11/2020.
+//
+
+#ifndef CS411_DP_BUILD_HPP
+#define CS411_DP_BUILD_HPP
+
+#endif //CS411_DP_BUILD_HPP
