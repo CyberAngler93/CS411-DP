8217f5d26886bb4aa2887b8183aa7b2b3bcc14de
CyberAngler93 <mperry37@alaska.edu>
2020-11-11 18:32:27 -0900

    feat: update functions used.
    Removes the need for compare.

--- build.hpp
+++ build.hpp
@@ -4,5 +4,11 @@
 
 #ifndef CS411_DP_BUILD_HPP
 #define CS411_DP_BUILD_HPP
+#include <vector>
+#include <algorithm>
+#include <cmath>
 
+using Bridge = std::vector<int>;
+int build(int , int , const std::vector<Bridge> &);
+int recurse_bridge(const std::vector<Bridge> &, int, int, std::vector<std::vector<int>> &);
 #endif //CS411_DP_BUILD_HPP
